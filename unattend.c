9ca1d029df0457df987cfff06d6c180afe623e6f
Sean Fisk <sean@seanfisk.com>
2023-10-17 22:27:13 -0400

    wip

--- unattend.c
+++ unattend.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 #include <IOKit/pwr_mgt/IOPMLib.h>
-#include <IOKit/pwr_mgt/IOPMLibPrivate.h>
-
+/* #include <IOKit/pwr_mgt/IOPMLibPrivate.h> */
 
 int main(int argc, char *argv[]) {
   puts("Hello, world!");
 
   IOPMAssertionID assertionID;
   const IOReturn result = IOPMAssertionCreateWithDescription(
-                                                             /*AssertionType=*/kIOPMAssertionTypePreventUserIdleSystemSleep,
-                                                             /*Name=*/CFSTR("unattend command-line tool"),
-                                                             /*Details=*/CFSTR("test assertion"),
-                                                             /*HumanReadableReason=*/CFSTR("Run time-consuming operation in background"), /*LocalizationBundlePath=*/NULL,
-                                                             /*Timeout=*/0,
+      /*AssertionType=*/kIOPMAssertionTypePreventUserIdleSystemSleep,
+      /*Name=*/CFSTR("unattend command-line tool"),
+      /*Details=*/CFSTR("test assertion"),
+      /*HumanReadableReason=*/
+      CFSTR("Run time-consuming operation in background"),
+      /*LocalizationBundlePath=*/NULL,
+      /*Timeout=*/0,
 
-                                                             // I think this differs from kIOPMAssertionTimeoutActionTurnOff in that the assertion is deleted, not just made inactive
-                                                             /*TimeoutAction=*/kIOPMAssertionTimeoutActionRelease,
-                                                             &assertionID);
+      // I think this differs from kIOPMAssertionTimeoutActionTurnOff in that
+      // the assertion is deleted, not just made inactive
+      /*TimeoutAction=*/kIOPMAssertionTimeoutActionRelease, &assertionID);
 
   if (result != kIOReturnSuccess) {
-    fputs(stderr, "Failed to create power assertion");
+    fputs("Failed to create power assertion", stderr);
   }
 
   return 0;
