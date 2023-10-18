5956aa9536d49436f281a8394bcfc5c03404d451
Sean Fisk <sean@seanfisk.com>
2023-10-17 22:54:08 -0400

    trying with createwithname

--- unattend.c
+++ unattend.c
@@ -1,23 +1,37 @@
+#include <IOKit/pwr_mgt/IOPMLib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <IOKit/pwr_mgt/IOPMLib.h>
 
 int main(int argc, char *argv[]) {
   puts("Hello, world!");
 
-  IOPMAssertionID assertionID;
-  const IOReturn result = IOPMAssertionCreateWithDescription(
-      /*AssertionType=*/kIOPMAssertionTypePreventUserIdleSystemSleep,
-      /*Name=*/CFSTR("unattend command-line tool"),
-      /*Details=*/CFSTR("test assertion"),
-      /*HumanReadableReason=*/
-      CFSTR("Run time-consuming operation in background"),
-      /*LocalizationBundlePath=*/NULL,
-      /*Timeout=*/0,
+  IOPMAssertionID assertion_id;
 
-      // I think this differs from kIOPMAssertionTimeoutActionTurnOff in that
-      // the assertion is deleted, not just made inactive
-      /*TimeoutAction=*/kIOPMAssertionTimeoutActionRelease, &assertionID);
+  // See here for the difference between assertion types:
+  // https://stackoverflow.com/a/66093676
+
+  const CFStringRef assertion_type =
+      kIOPMAssertionTypePreventUserIdleSystemSleep;
+
+  // Note: Limited to 128 characters
+  const CFStringRef name = CFSTR("unattend command-line tool");
+
+  IOReturn result;
+  /* result = IOPMAssertionCreateWithDescription( */
+  /*     assertion_type, name, */
+  /*     /\*Details=*\/CFSTR("test assertion"), */
+  /*     /\*HumanReadableReason=*\/ */
+  /*     CFSTR("Run time-consuming operation in background"), */
+  /*     /\*LocalizationBundlePath=*\/NULL, */
+  /*     /\*Timeout=*\/0, */
+
+  /*     // I think this differs from kIOPMAssertionTimeoutActionTurnOff in that */
+  /*     // the assertion is deleted, not just made inactive */
+  /*     /\*TimeoutAction=*\/kIOPMAssertionTimeoutActionRelease, &assertion_id); */
+
+  result = IOPMAssertionCreateWithName(
+      assertion_type,
+      /*AssertionLevel=*/kIOPMAssertionLevelOff, name & assertion_id);
 
   sleep(30 /*seconds*/);
 
